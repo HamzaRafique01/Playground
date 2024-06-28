e7a4f2e6c5b4c13960baa2a562ffed6fd09bb6da
HamzaRafique01 <hamza.rafique@devigital.com>
2024-06-28 22:21:26 +0500

    Fixed Email Patteren

--- randomnumbergen.ts
+++ randomnumbergen.ts
@@ -1,6 +1,7 @@
 export function generateUniqueEmail(baseEmail: string): string {
     const randomNumber = Math.floor(Math.random() * 1000000);
     const [username, domain] = baseEmail.split("@");
-    const uniqueEmail = `${username}+${randomNumber}@${domain}`;
+    const uniqueEmail = `${username}${randomNumber}@${domain}`;
+    console.log(uniqueEmail);
     return uniqueEmail;
   }
